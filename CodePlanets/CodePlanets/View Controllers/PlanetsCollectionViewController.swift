bdf093a39ca47c7b484de2e404bab1152e73a754
Stephanie Ballard <user.name>
2020-04-13 11:25:13 -0700

    finished

--- CodePlanets/CodePlanets/View Controllers/PlanetsCollectionViewController.swift
+++ CodePlanets/CodePlanets/View Controllers/PlanetsCollectionViewController.swift
@@ -8,35 +8,62 @@
 
 import UIKit
 
-class PlanetsCollectionViewController: UICollectionViewController {
-
+class PlanetsCollectionViewController: UIViewController, UICollectionViewDataSource {
+    
     // MARK: - Properties
     let planetController = PlanetController()
+    private var collectionView: UICollectionView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
+        view.backgroundColor = .white
+        title = "Our Planets"
+        
+        configureCollectionView()
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         collectionView?.reloadData()
     }
-
+    
+    private func configureCollectionView() {
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 20, left: 10, bottom: 10, right: 10)
+        layout.itemSize = CGSize(width: 160, height: 190)
+        layout.scrollDirection = .vertical
+        layout.minimumInteritemSpacing = 10
+        layout.minimumLineSpacing = 10
+        
+        let collectionView = UICollectionView(frame: view.frame, collectionViewLayout: layout)
+        
+        view.addSubview(collectionView)
+        
+        collectionView.register(PlanetCollectionViewCell.self, forCellWithReuseIdentifier: "PlanetCell")
+        
+        collectionView.dataSource = self
+        collectionView.backgroundColor = .white
+        
+        self.collectionView = collectionView
+    }
+    
     // MARK: - UICollectionViewDataSource
-
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return planetController.planets.count
     }
-
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PlanetCell", for: indexPath) as! PlanetCollectionViewCell
-
+        
         let planet = planetController.planets[indexPath.item]
         
         cell.planet = planet
-    
+        
         return cell
     }
-
+    
     // MARK: - Action Handlers
     
-    @IBAction func unwindToPlanetsCollectionViewController(_ sender: UIStoryboardSegue) {
-    }
-
 }
--- CodePlanets/CodePlanets/Views/PlanetCollectionViewCell.swift
+++ CodePlanets/CodePlanets/Views/PlanetCollectionViewCell.swift
@@ -10,13 +10,90 @@ import UIKit
 
 class PlanetCollectionViewCell: UICollectionViewCell {
     
-    @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var nameLabel: UILabel!
+    private var imageView = UIImageView()
+    private var nameLabel = UILabel()
     
     var planet: Planet? {
         didSet {
             updateViews()
         }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUpSubviews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUpSubviews() {
+        // configure imageview and use NSLayoutContraint or NSLayoutAnchor to add constriaints to the imageview
+        
+        // 1. create/configure subview
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        
+        // 2. add subview to view hierarchy
+        addSubview(imageView)
+        
+        // 3. create constraints
+        
+        // This is the y constraint
+        let imageViewYConstraint = NSLayoutConstraint(item: imageView,
+                                                      attribute: .top,
+                                                      relatedBy: .equal,
+                                                      toItem: self,
+                                                      attribute: .top,
+                                                      multiplier: 1.0,
+                                                      constant: 4)
+        // This is the x constraint
+        let imageViewXConstraint = NSLayoutConstraint(item: imageView,
+                                                      attribute: .leading,
+                                                      relatedBy: .equal,
+                                                      toItem: self,
+                                                      attribute: .leading,
+                                                      multiplier: 1.0,
+                                                      constant: 4)
+        // This is the width constraint
+        let imageViewWidthConstraint = NSLayoutConstraint(item: imageView,
+                                                          attribute: .trailing,
+                                                          relatedBy: .equal,
+                                                          toItem: self,
+                                                          attribute: .trailing,
+                                                          multiplier: 1.0,
+                                                          constant: -4)
+        // This is height constraint
+        let imageViewHeightConstraint = NSLayoutConstraint(item: imageView,
+                                                           attribute: .height,
+                                                           relatedBy: .equal,
+                                                           toItem: imageView,
+                                                           attribute: .width,
+                                                           multiplier: 1,
+                                                           constant: 0)
+        
+        // 4. activate constraints
+        NSLayoutConstraint.activate([imageViewXConstraint, imageViewYConstraint, imageViewWidthConstraint, imageViewHeightConstraint])
+        
+        // configure the label and use the same constraint classes to make it appear centered and below the imageview
+        
+        // 1.
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        nameLabel.textAlignment = .center
+        
+        // 2.
+        addSubview(nameLabel)
+        
+        // 3.
+        
+        // This is the y constraint
+        nameLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 4).isActive = true
+        // This is the x constraint
+        nameLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 2).isActive = true
+        // This is the width constraint
+        nameLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -2).isActive = true
     }
     
     private func updateViews() {
